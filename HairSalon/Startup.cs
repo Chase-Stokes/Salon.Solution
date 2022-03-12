ce1e2b293b6f0454c0ebc702279aac4176a394a3
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-11 16:31:14 -0800

    FINALLY GOT MY STYLES TO WORK, add styling to the views

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -1,10 +1,60 @@
-using HairSalon.Models;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.EntityFrameworkCore;
+// using HairSalon.Models;
+
+// namespace HairSalon
+// {
+//   public class Startup
+//   {
+//     public Startup(IWebHostEnvironment env)
+//     {
+//       var builder = new ConfigurationBuilder()
+//           .SetBasePath(env.ContentRootPath)
+//           .AddJsonFile("appsettings.json");
+//       Configuration = builder.Build();
+//     }
+
+//     public IConfigurationRoot Configuration { get; set; }
+
+//     public void ConfigureServices(IServiceCollection services)
+//     {
+//       services.AddMvc();
+//       services.AddEntityFrameworkMySql()
+//         .AddDbContext<HairSalonContext>(options => options
+//         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+//     }
+
+//     public void Configure(IApplicationBuilder app)
+//     {
+//       app.UseDeveloperExceptionPage();
+//       app.UseRouting();
+//       app.UseStaticFiles();
+
+//       app.UseEndpoints(routes =>
+//       {
+//         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+//       });
+
+//       app.Run(async (context) =>
+//       {
+//         await context.Response.WriteAsync("Hello World!");
+//       });
+//     }
+
+//   }
+// }
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
 
 namespace HairSalon
 {
@@ -14,7 +64,7 @@ namespace HairSalon
     {
       var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json");
+          .AddJsonFile("appsettings.json"); 
       Configuration = builder.Build();
     }
 
@@ -23,6 +73,7 @@ namespace HairSalon
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
       services.AddEntityFrameworkMySql()
         .AddDbContext<HairSalonContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
@@ -31,8 +82,8 @@ namespace HairSalon
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
-      app.UseRouting();
       app.UseStaticFiles();
+      app.UseRouting();
 
       app.UseEndpoints(routes =>
       {
@@ -44,6 +95,5 @@ namespace HairSalon
         await context.Response.WriteAsync("Hello World!");
       });
     }
-
   }
 }
